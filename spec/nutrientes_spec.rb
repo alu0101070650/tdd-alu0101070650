2dd13a124183cd8c36e70ac87cd7a1dbfc1b8a85
Andres Garcia Perez <alu0101070650@ull.edu.es>
2017-10-18 22:35:39 +0100

    Añadida la especificación: debe existir un metodo para obtener el alimento formateado

--- spec/nutrientes_spec.rb
+++ spec/nutrientes_spec.rb
@@ -68,4 +68,16 @@ RSpec.describe Alimento do
       end
     end
   end
+
+  describe "#to_s" do
+    context "en el alimento Pollo" do
+      it "devuelve Pollo (proteínas: 20.6, glúcidos: 0, lípidos: 5.6)" do
+        alimento = Alimento.new("Pollo")
+        alimento.proteinas = 20.6
+        alimento.glucidos = 0
+        alimento.lipidos = 5.6
+        expect(alimento.grasas).to eq("Pollo (proteínas: 20.6, glúcidos: 0, lípidos: 5.6)")
+      end
+    end
+  end
 end
