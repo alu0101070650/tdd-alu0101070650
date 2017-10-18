0d59212ab9887ef1a7b2159c9d7fe82d4c20bbdc
Andres Garcia Perez <alu0101070650@ull.edu.es>
2017-10-18 22:10:59 +0100

    Añadida la especificación: debe existir un metodo para obtener el nombre del alimento

--- spec/nutrientes_spec.rb
+++ spec/nutrientes_spec.rb
@@ -21,12 +21,21 @@ RSpec.describe Alimento do
   it "existe cantidad de glúcidos" do
     alimento = Alimento.new("Pollo")
     expect(alimento.instance_variable_defined?(:@glucidos)).to be true
-    expect(alimento.respond_to?("@glucidos=")).to be true
+    expect(alimento.respond_to?("glucidos=")).to be true
   end
 
   it "existe cantidad de grasas" do
     alimento = Alimento.new("Pollo")
     expect(alimento.instance_variable_defined?(:@grasas)).to be true
     expect(alimento.respond_to?("grasas=")).to be true
+  end
+
+  describe "#nombre" do
+    context "en el alimento Pollo" do
+      it "devuelve Pollo" do
+        alimento = Alimento.new("Pollo")
+        expect(alimento.nombre).to eq("Pollo")
+      end
+    end
   end
 end
