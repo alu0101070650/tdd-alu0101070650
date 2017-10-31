156cf77ed4f5940e16ea20b9afe31d829d01a48c
Andres Garcia Perez <alu0101070650@ull.edu.es>
2017-10-31 17:59:11 +0000

    Implementada la especificación: se puede insertar un elemento en la lista

--- lib/nutrientes/lista.rb
+++ lib/nutrientes/lista.rb
@@ -4,4 +4,17 @@ class Lista
     @cabeza = nil
     @cola = nil
   end
+
+  def insertar(elemento)
+    nodo = Nodo.new
+    nodo.valor = elemento
+    if @cabeza.nil?
+      @cabeza = nodo
+      @cola = nodo
+    else
+      @cola.siguiente = nodo
+      nodo.previo = @cola
+      @cola = nodo
+    end
+  end
 end
