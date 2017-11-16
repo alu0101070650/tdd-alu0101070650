deff923f853d7096172f7870858dde697a3c7e7c
Andres Garcia Perez <alu0101070650@ull.edu.es>
2017-11-16 13:35:28 +0000

    Añadida especificación: el alimento debe poder calcular el IG

--- spec/nutrientes_spec.rb
+++ spec/nutrientes_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe Nutrientes do
       expect(Lista < Enumerable).to be true
       lista.insertar_a(["Hola", "que", "tal"])
       a = []
-      lista.each{|x| a << x}
+      lista.each {|x| a << x}
       expect(a == ["Hola", "que", "tal"]).to be true
     end
 
@@ -331,7 +331,7 @@ RSpec.describe Nutrientes do
       it "es un Alimento" do
         expect(@alimento.is_a?(Alimento)).to be true
       end
-      context "para el Manzana" do
+      context "para la Manzana" do
         describe "#valor_energetico" do
           it "devuelve 54.4 Kcal" do
             alimento = Fruta.new("Manzana")
@@ -503,6 +503,41 @@ RSpec.describe Nutrientes do
           alimento.glucidos = 0.2
           alimento.grasas = 99.6
           expect(alimento.valor_energetico).to be_within(@tolerancia).of(897.2)
+        end
+      end
+    end
+
+    describe "#indice_glucemico" do
+      before :each do
+        @tolerancia = 1
+        @glucosa_individuos = [[4.9, 5.3, 5.9, 6.7, 7.2, 7.6, 8.0, 8.2, 8.2, 8.4, 8.3, 8.3, 8.0, 7.5, 7.1, 6.8, 6.8, 6.9, 6.8, 6.3, 6.2, 6.3, 6.2, 6.3, 6.1], [6.3, 5.4, 5.6, 5.7, 6.5, 7.4, 7.9, 7.4, 7.7, 7.9, 7.9, 7.8, 7.8, 7.8, 8.0, 8.5, 9.4, 10.8, 10.5, 9.1, 8.9, 8.3, 7.7, 7.6, 7.5]]
+        @delta_tiempo = 5
+      end
+
+      context "en el alimento chocolate" do
+        it "devuelve 13" do
+          alimento = Alimento.new("Chocolate")
+          mediciones_alimento = [[6.5, 6.5, 6.7, 6.5, 6.5, 6.8, 6.7, 6.2, 6.5, 7.2, 6.9, 7.0, 6.3, 6.2, 6.1, 5.9, 5.8, 6.1, 6.7, 6.7, 6.6, 6.7, 6.9, 7.2, 7.1], [4.6, 4.6, 4.7, 4.7, 4.8, 4.7, 4.8, 4.8, 4.6, 4.4, 4.7, 4.7, 4.8, 4.7, 5.2, 5.9, 5.9, 5.7, 5.4, 5.3, 5.1, 4.8, 4.8, 4.9, 5.9]]
+          alimento.mediciones_alimento=mediciones_alimento
+          expect(alimento.indice_glucemico(@delta_tiempo, @glucosa_individuos)).to be_within(@tolerancia).of(13)
+        end
+      end
+
+      context "en el alimento yogurt" do
+        it "devuelve 41" do
+          alimento = Alimento.new("Yogurt")
+          mediciones_alimento = [[6.1, 6.6, 6.3, 6.3, 6.1, 6.9, 6.8, 6.5, 6.4, 6.9, 6.8, 6.5, 6.3, 6.2, 6.7, 6.2, 5.9, 5.8, 5.8, 5.8, 5.8, 5.8, 5.9, 6.2, 6.4], [4.9, 4.9, 5.2, 5.8, 6.5, 7.0, 7.2, 7.3, 7.3, 6.6, 6.2, 6.1, 6.0, 6.1, 5.9, 5.9, 5.9, 5.9, 5.8, 5.8, 5.5, 5.5, 5.6, 5.9, 5.9]]
+          alimento.mediciones_alimento=mediciones_alimento
+          expect(alimento.indice_glucemico(@delta_tiempo, @glucosa_individuos)).to be_within(@tolerancia).of(41)
+        end
+      end
+
+      context "en el alimento compota de manzana" do
+        it "devuelve 54" do
+          alimento = Alimento.new("Compota de manzana")
+          mediciones_alimento = [[6.7, 6.5, 6.8, 6.9, 7.0, 7.1, 6.9, 6.9, 6.9, 6.7, 6.9, 7.3, 7.0, 7.0, 7.2, 7.1, 6.8, 7.2, 7.3, 7.0, 6.8, 6.7, 6.8, 6.7, 6.9], [4.6, 4.8, 5.3, 5.6, 6.1, 6.5, 6.6, 7.0, 7.0, 6.8, 6.4, 6.3, 6.1, 6.1, 6.2, 6.0, 6.1, 6.1, 6.2, 6.3, 6.4, 6.1, 6.1, 5.7, 5.9]]
+          alimento.mediciones_alimento=mediciones_alimento
+          expect(alimento.indice_glucemico(@delta_tiempo, @glucosa_individuos)).to be_within(@tolerancia).of(54)
         end
       end
     end
