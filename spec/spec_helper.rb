b1655f19930e5b57906cc88464e4118fd93b5978
Andres Garcia Perez <alu0101070650@ull.edu.es>
2017-11-23 20:59:04 +0000

    Corregido require de Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
-require "bundler/setup"
-require "nutrientes"
 require "coveralls"
 Coveralls.wear!
+require "bundler/setup"
+require "nutrientes"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
